6624d6aeb62daaf64e8c69f199b86dff6fe9f505
Niklesh Shakya <uniqsaqya@gmail.com>
2019-05-13 23:49:17 +0545

    font awesome

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { FaHome } from "react-icons/fa";
 export default class App extends Component {
   render() {
     return <div>Hello from techstore</div>;
