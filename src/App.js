ea7446ee8e7bc51b598930515f76baa3aba24221
Niklesh Shakya <uniqsaqya@gmail.com>
2019-05-13 23:56:23 +0545

    styled components

--- src/App.js
+++ src/App.js
@@ -2,8 +2,21 @@ import React, { Component } from "react";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { FaHome } from "react-icons/fa";
+import styled from "styled-components";
+
 export default class App extends Component {
   render() {
-    return <div>Hello from techstore</div>;
+    return (
+      <div>
+        Hello from techstore
+        <Button>Hello styled components</Button>
+      </div>
+    );
   }
 }
+
+const Button = styled.button`
+  color: red;
+  background: blue;
+  font-size: 2rem;
+`;
